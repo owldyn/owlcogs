b41165a5994e413a97f98d941932e0006bf38ce2
owldyn <owldyn@gmail.com>
2021-09-04 09:15:26 +0000

    Added some comments, some changes to vidlink to use webm as to support youtube videos

--- genericdl/genericdl.py
+++ genericdl/genericdl.py
@@ -23,23 +23,24 @@ class Genericdl(commands.Cog):
             if url[0] == '<':
                 url = url[1:len(url)-1]
 
-            fname = '/tmp/{}.mp4'.format(ctx.message.id)
+            fname = '/tmp/{}.webm'.format(ctx.message.id)
 
             if url.find(".") >= 0:
                 try:
-                    subprocess.run(['youtube-dl', url, '-o', fname])
-                    titleraw = subprocess.run(['youtube-dl', '--get-title', url], capture_output=True)
+                    subprocess.run(['yt-dlp', url, '-o', fname, '--merge-output-format', 'webm'])
+                    titleraw = subprocess.run(['yt-dlp', '--get-title', url], capture_output=True)
                     title = titleraw.stdout.decode("utf-8")[0:len(titleraw.stdout)-1]
 
                     fs = os.stat(fname).st_size
                     if fs < 8388119:
                         stream=io.open(fname, "rb")
-                        await ctx.send(content="Title: {}".format(title), file=discord.File(stream, filename="{}.mp4".format(title)))
+                        await ctx.send(content="Title: {}".format(title), file=discord.File(stream, filename="{}.webm".format(title)))
                         os.remove(fname)
                     else:
                         await ctx.send("File too large. Use link to watch.")
-                except:
+                except Exception as e:
                     await ctx.send("Hoot! Error occured. Perhaps youtube-dl is broke with this website?")
+                    await ctx.send(f'Error is: {str(e)}')
                     pass
                     
             else:
@@ -75,9 +76,27 @@ class Genericdl(commands.Cog):
                 await ctx.send("{} is not a valid link.".format(url))
     
     async def calc_bitrate(self, duration, filesize, audio_size):
-        audio_size = (int(audio_size) + 32) * 1024 # Round up a little to account for variation
+        """Returns the bitrate ( in kilobits ) for a given string containing the duration, and the given max filesize (in bytes)
+        Duration string should contain the duration with "Duration: 00:00:00.00"
+        Will lower bitrate by audio_size + 22 * 1024 to account for variation and audio."""
+        audio_size = (int(audio_size) + 22) * 1024 # Round up a little to account for variation
         filesize = int(filesize) * 8
         bitrate = int((filesize / int(duration)) - audio_size)
+        return int(bitrate / 1024)
+
+    async def calc_bitrate2(self, file_string, filesize):
+        """Returns the bitrate ( in kilobits ) for a given string containing the duration, and the given max filesize (in bytes)
+        Duration string should contain the duration with "Duration: 00:00:00.00"
+        Will lower bitrate by 150Kb to account for variation and audio."""
+        audio_size = 150 * 1024 # Rounded up a little to account for variation
+        raw_duration_regex = re.search(r'Duration: \d\d:\d\d:\d\d.\d\d', file_string)
+        raw_duration_regex = re.search(r'\d\d:\d\d:\d\d', raw_duration_regex.group(0))
+        duration_array = raw_duration_regex.group(0).split(":")
+        duration = int(duration_array[0]) * 60 * 60
+        duration += int(duration_array[1]) * 60
+        duration += int(duration_array[2])
+        filesize = filesize * 8
+        bitrate = int((filesize / duration) - audio_size)
         return int(bitrate / 1024)
 
     @commands.command()
