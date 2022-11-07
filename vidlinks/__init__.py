e3a2c7f923ef03ab58d0d5eac41ebbbc59c0660f
owldyn <owldyn@gmail.com>
2022-11-07 04:23:51 +0000

    setting up a generic link handler

---
+++ vidlinks/__init__.py
@@ -0,0 +1,4 @@
+from .vidlinks import VidLink
+
+def setup(bot):
+    bot.add_cog(VidLink(bot))
---
+++ vidlinks/processors/__init__.py
@@ -0,0 +1,5 @@
+from . import *
+from enum import Enum
+
+class Processors(Enum):
+    """The processors that can process a given link website"""
