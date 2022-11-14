534551453bbb4c3f9e5f2b81dd6a9e45c9e75121
owldyn <owldyn@gmail.com>
2022-11-14 02:30:34 +0000

    create class to handle ffmpeg commands on files

--- vidlinks/processors/libraries/ffmpeg.py
+++ vidlinks/processors/libraries/ffmpeg.py
@@ -1,41 +1,73 @@
+import json
 import subprocess
 import tempfile
-import json
+from enum import Enum
+
 
 class Ffmpeg:
-    def _run_process_on_file(self, command, file):
+    @staticmethod
+    def _run_process_on_file(command, file):
         with subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE) as process:
             # Seek the file to prep it for piping
             file.seek(0)
             return process.communicate(input=file.read())
 
-    def get_information(self, file: tempfile.SpooledTemporaryFile):
+    def get_information(self, file: tempfile.SpooledTemporaryFile) -> dict:
         """Gets a json of the information of the file."""
-        command = ['ffprobe']
         args = ['-v', 'quiet', '-print_format', 'json', '-show_format', '-show_streams', '-']
-        command.extend(args)
-        
-        json_string = self._run_process_on_file(command, file)[0].decode('utf-8')
+        json_string = self.run_ffmpeg_command_on_file(self.Commands.FFPROBE, args, file)[0].decode('utf-8')
         return json.loads(json_string)
 
-    def check_for_audio(self, file_information: dict = None) -> bool:
-        """Checks if there is audio in the file"""
+    @staticmethod
+    def check_for_audio(file_information: dict) -> bool:
+        """Checks if there is audio in the file from the given file information"""
         streams = file_information.get('streams', [])
         for stream in streams:
             if stream.get('codec_type', '') == 'audio':
                 return True
         return False
 
+    class Commands(Enum):
+        """The commands to run ffmpeg with"""
+        FFMPEG = ['ffmpeg']
+        FFPROBE = ['ffprobe']
+
+    def run_ffmpeg_command_on_file(self, command: Commands, 
+                                   args: list, file: tempfile.SpooledTemporaryFile) -> tuple:
+        """Runs the command with the given args on the given file.
+
+        Args:
+            command (Commands): The command to run
+            args (list): The args to add to the command
+            file (tempfile.SpooledTemporaryFile): The file to run it on.
+
+        Returns:
+            tuple: the return from subprocess.popen.communicate
+        """
+        command = command.value
+        command.extend(args)
+        return self._run_process_on_file(command, file)
+    
+    @staticmethod
+    def _replace_file(original_file: tempfile.SpooledTemporaryFile, new_data):
+        original_file.seek(0)
+        original_file.truncate()
+        original_file.write(new_data)
+
     def replace_audio(self, file: tempfile.SpooledTemporaryFile):
         """Replaces the file's audio with silent audio. Works even if there's no audio track.
         Modifies the file object in place."""
-        command = ['ffmpeg']
         args = ['-i', '-', '-f', 'lavfi', '-i', 'anullsrc=channel_layout=stereo:sample_rate=44100',
         '-c:v', 'copy', '-c:a', 'aac', '-map', '0:v', '-map', '1:a', '-shortest', '-f', 'mp4', '-']
-        command.extend(args)
 
-        removed_audio = self._run_process_on_file(command, file)[0]
+        removed_audio = self.run_ffmpeg_command_on_file(self.Commands.FFMPEG ,args, file)[0]
         # Erase the file and replace it with the new file.
-        file.seek(0)
-        file.truncate()
-        file.write(removed_audio)
+        self._replace_file(file, removed_audio)
+
+    def lower_quality(self, file: tempfile.SpooledTemporaryFile):
+        """Lowers the quality of the video by halving the resolution on both axis."""
+        args = ['ffmpeg', '-i', '-', '-crf', '24', '-vf',
+                'scale=ceil(iw/4)*2:ceil(ih/4)*2', '-c:a', 'copy', '-f', 'mp4', '-']
+
+        smaller_video = self.run_ffmpeg_command_on_file(self.Commands.FFMPEG, args, file)
+        self._replace_file(file, smaller_video)
