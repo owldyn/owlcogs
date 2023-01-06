24731d8cf02e20e8c7238d079e93530a47ee2011
owldyn <owldyn@gmail.com>
2023-01-06 05:32:58 +0000

    update ffmpeg to use 2 tempfiles
    This fixes the infinite load issue on mobile video previews...

--- medialinks/processors/libraries/ffmpeg.py
+++ medialinks/processors/libraries/ffmpeg.py
@@ -2,21 +2,22 @@ import json
 import subprocess
 import tempfile
 from enum import Enum
-
-
+import logging
+logger = logging.getLogger(
+            "owldyn.vidlinks.library.ffmpeg")
 class Ffmpeg:
-    ffmpeg_always_args = ['-movflags', 'frag_keyframe+empty_moov',
-                          '-bsf:a', 'aac_adtstoasc', '-f', 'mp4', '-']
+    ffmpeg_always_args = ['-y', '-f', 'mp4']
     def __init__(self) -> None: #TODO Convert this to ask for the file to begin with.
         """Class for handling ffmpeg calls.
         Make one instance per video file!"""
         self._information = None
     @staticmethod
-    def _run_process_on_file(command, file):
+    def _run_process_on_file(command):
         # with subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE) as process:
         #     # Seek the file to prep it for piping
         #     file.seek(0)
         #     info = process.communicate(input=file.read())
+        logger.debug(f"Running ffmpeg command {command}")
         try:
             info = subprocess.run(command, capture_output=True, check=False)
             info.check_returncode()
@@ -30,8 +31,8 @@ class Ffmpeg:
         if self._information is None:
             args = ['-v', 'quiet', '-print_format', 'json',
                     '-show_format', '-show_streams', file.name]
-            json_string = self.run_ffmpeg_command_on_file(
-                self.Commands.FFPROBE, args, file)[0].decode('utf-8')
+            json_string = self.run_ffprobe_command_on_file(
+                self.Commands.FFPROBE, args)[0].decode('utf-8')
             try:
                 return json.loads(json_string)
             except json.JSONDecodeError as exc:
@@ -56,8 +57,8 @@ class Ffmpeg:
         """Errors called in the FFMPEG library"""
 
         def __init__(self, file, *args: object) -> None:
-            self.ffmpeg_response = Ffmpeg().run_ffmpeg_command_on_file(
-                Ffmpeg.Commands.FFPROBE, [file.name], file)
+            self.ffmpeg_response = Ffmpeg().run_ffprobe_command_on_file(
+                Ffmpeg.Commands.FFPROBE, [file.name])
             super().__init__(str(self.ffmpeg_response), *args)
 
     def run_ffmpeg_command_on_file(self, command: Commands,
@@ -74,15 +75,38 @@ class Ffmpeg:
         """
         commandlist = [command.value]
         commandlist.extend(args)
+        with tempfile.NamedTemporaryFile() as intermediate:
+            if command == self.Commands.FFMPEG:
+                commandlist.extend(self.ffmpeg_always_args)
+                commandlist.append(intermediate.name)
+            self._run_process_on_file(commandlist)
+            self._replace_file(file, intermediate)
+
+
+    def run_ffprobe_command_on_file(self, command: Commands,
+                                   args: list) -> tuple:
+        """Runs the command with the given args on the given file.
+
+        Args:
+            command (Commands): The command to run
+            args (list): The args to add to the command
+            file (tempfile.NamedTemporaryFile): The file to run it on.
+
+        Returns:
+            tuple: the return from subprocess.popen.communicate
+        """
+        commandlist = [command.value]
+        commandlist.extend(args)
         if command == self.Commands.FFMPEG:
             commandlist.extend(self.ffmpeg_always_args)
-        return self._run_process_on_file(commandlist, file)
+        return self._run_process_on_file(commandlist)
 
     @staticmethod
-    def _replace_file(original_file: tempfile.NamedTemporaryFile, new_data):
+    def _replace_file(original_file: tempfile.NamedTemporaryFile, new_file: tempfile.NamedTemporaryFile):
+        new_file.seek(0)
         original_file.seek(0)
         original_file.truncate()
-        original_file.write(new_data)
+        original_file.write(new_file.read())
 
     def replace_audio(self, file: tempfile.NamedTemporaryFile):
         """Replaces the file's audio with silent audio. Works even if there's no audio track.
@@ -90,41 +114,36 @@ class Ffmpeg:
         args = ['-i', file.name, '-f', 'lavfi', '-i', 'anullsrc=channel_layout=stereo:sample_rate=44100',
                 '-c:v', 'copy', '-c:a', 'aac', '-map', '0:v', '-map', '1:a', '-shortest']
 
-        removed_audio = self.run_ffmpeg_command_on_file(
-            self.Commands.FFMPEG, args, file)[0]
-        # Erase the file and replace it with the new file.
-        self._replace_file(file, removed_audio)
+        self.run_ffmpeg_command_on_file(
+            self.Commands.FFMPEG, args, file)
+
 
     def shrink_video(self, file: tempfile.NamedTemporaryFile):
         """Lowers the quality of the video by halving the resolution on both axis."""
         args = ['-i', file.name, '-crf', '24', '-vf',
                 'scale=ceil(iw/4)*2:ceil(ih/4)*2', '-b:a', '128k']
 
-        smaller_video = self.run_ffmpeg_command_on_file(
-            self.Commands.FFMPEG, args, file)[0]
-        self._replace_file(file, smaller_video)
+        self.run_ffmpeg_command_on_file(
+            self.Commands.FFMPEG, args, file)
 
     def lower_quality(self, file: tempfile.NamedTemporaryFile):
         """Lowers the quality of the video by using crf 28."""
         args = ['-i', file.name, '-preset',
                 'veryfast', '-crf', '28', '-b:a', '128k']
 
-        smaller_video = self.run_ffmpeg_command_on_file(
-            self.Commands.FFMPEG, args, file)[0]
-        self._replace_file(file, smaller_video)
+        self.run_ffmpeg_command_on_file(
+            self.Commands.FFMPEG, args, file)
 
     def convert_to_mp4(self, file: tempfile.NamedTemporaryFile):
         """Converts file to mp4"""
         args = ['-i', file.name, '-c:v', 'libx264', '-c:a', 'copy']
-        video = self.run_ffmpeg_command_on_file(
-            self.Commands.FFMPEG, args, file)[0]
-        self._replace_file(file, video)
+        self.run_ffmpeg_command_on_file(
+            self.Commands.FFMPEG, args, file)
 
     def normalize_file(self, file: tempfile.NamedTemporaryFile):
         """Runs the file through ffmpeg with copy codecs
         to fix any issues caused by the way we call yt-dlp."""
         args = ['-i', file.name, '-c:v', 'copy', '-c:a', 'copy']
 
-        normalized_video = self.run_ffmpeg_command_on_file(
-            self.Commands.FFMPEG, args, file)[0]
-        self._replace_file(file, normalized_video)
+        self.run_ffmpeg_command_on_file(
+            self.Commands.FFMPEG, args, file)
