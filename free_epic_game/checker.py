d10ebf6c4b4518b810d6e6ab3bae629130e530ef
owldyn <owldyn@gmail.com>
2023-06-26 00:55:31 +0000

    add a notifier for free epic games

--- free_epic_game/checker.py
+++ free_epic_game/checker.py
@@ -1,7 +1,14 @@
+import logging
+from datetime import datetime
+
 import discord
-from redbot.core import Config, commands
-from redbot.core.commands import Context
+from discord.ext import tasks
 from epicstore_api import EpicGamesStoreAPI
+from redbot.core import Config, app_commands, commands
+from redbot.core.bot import Red
+from redbot.core.commands import Context
+
+from .dataclasses import GameInfo
 
 
 class EpicGamesChecker(commands.Cog):
@@ -10,13 +17,105 @@ class EpicGamesChecker(commands.Cog):
 
     CHECK_MARK = "✅"
     default_global_settings = {
+        "channels": [],
         "history": {
-            "last_check": [],
+            "last_check": {},
             "historical": {},
-        }
+        },
     }
 
     def __init__(self, bot):
-        self.bot = bot
+        self.log = logging.getLogger(self.__class__.__name__)
+        self.bot: Red = bot
         self.conf = Config.get_conf(self, identifier=26400736017)
         self.conf.register_global(**self.default_global_settings)
+        self.epic_fail_count = 0
+        self.check.start()
+
+    async def cog_unload(self):
+        """unload"""
+        self.check.cancel()
+
+    free_epic = app_commands.Group(
+        name="free_epic",
+        description="Free Epic Game Notifier",
+    )
+
+    @free_epic.command(
+        name="add_channel",
+        description="Add this channel to the list to notify.",
+    )
+    @app_commands.guild_only()
+    @app_commands.checks.has_permissions(manage_channels=True)
+    async def add_channel(self, ctx: discord.Interaction):
+        """Add this channel to the alert channels"""
+        async with self.conf.channels() as channels:
+            channels.append(ctx.channel.id)
+            channels = list(set(channels))
+
+        await ctx.response.send_message("Done!")
+
+    @free_epic.command(
+        name="remove_channel",
+        description="Remove this channel from the list to notify.",
+    )
+    @app_commands.guild_only()
+    @app_commands.checks.has_permissions(manage_channels=True)
+    async def remove_channel(self, ctx: discord.Interaction):
+        """Remove this channel from the alert channels"""
+        async with self.conf.channels() as channels, self.conf.history() as history:
+            try:
+                channels.remove(ctx.channel.id)
+                channels = list(set(channels))
+                history["last_check"].pop(str(ctx.channel.id), None)
+            except Exception:  # pylint: disable=broad-except
+                pass
+        await ctx.response.send_message("Done!")
+
+    @free_epic.command()
+    @commands.is_owner()
+    async def run_now(self, ctx: discord.Interaction):
+        await self.check()
+        await ctx.response.send_message("Done!", ephemeral=True)
+
+    async def _handle_fails(self):
+        """Handles epic failing"""
+        self.epic_fail_count += 1
+        self.log.warning("Error getting epic games!", exc_info=1)
+        if self.epic_fail_count >= 2:
+            await self.bot.send_to_owners(
+                f"Error getting epic games! Times errored: {self.epic_fail_count}"
+            )
+
+    @tasks.loop(hours=1)
+    async def check(self):
+        """Check epic for new games."""
+        self.log.debug("Checking Epic for free games!")
+        try:
+            epic = EpicGamesStoreAPI()
+            games = GameInfo.make_from_response(epic.get_free_games())
+            current_ids = [game.game_id for game in games]
+            async with self.conf.history() as history, self.conf.channels() as channels:
+                for channel_id in channels:
+                    for game in games:
+                        # Update the history and make sure it wasn't already sent.
+                        history["historical"][game.game_id] = {
+                            "last_seen": datetime.today().isoformat()
+                        }
+                        if game.game_id in history.get("last_check", {}).get(
+                            str(channel_id), []
+                        ):
+                            continue
+                        # If not found, send to the channel
+                        channel = self.bot.get_channel(channel_id)
+                        if not channel:
+                            self.log.info(
+                                "Channel %s did not exist, removing it.", channel_id
+                            )
+                            channels.remove(channel_id)
+                        await channel.send(embed=game.embed())
+                    history["last_check"][str(channel_id)] = current_ids
+            self.epic_fail_count = 0
+        except Exception:  # pylint: disable=broad-except
+            await self._handle_fails()
+        self.log.debug("Finished checking Epic for free games!")
---
+++ free_epic_game/dataclasses.py
@@ -0,0 +1,86 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import List
+
+from discord import Embed
+from jsonpath_ng import parse
+
+
+@dataclass
+class GameInfo:
+    """Info about the free games."""
+
+    game_id: str
+    title: str
+    desc: str
+    url: str
+    start_date: datetime
+    end_date: datetime
+    thumbnail_url: str
+
+    @classmethod
+    def make_from_response(cls, response) -> List["GameInfo"]:
+        """Builds a list of this class from the Epic free games response"""
+        games = (
+            response.get("data", {})
+            .get("Catalog")
+            .get("searchStore", {})
+            .get("elements", [])
+        )
+        infos = []
+        for game in games:
+            parser_now = parse(
+                "promotions.promotionalOffers.[*].promotionalOffers.[*].discountSetting.discountPercentage"
+            )
+            # Maybe do future stuff at some point?
+            # parser_future = parse("promotions.upcomingPromotionalOffers.[*].promotionalOffers.[*].discountSetting.discountPercentage")
+
+            if (
+                not parser_now
+                or 0 not in [g.value for g in parser_now.find(game)]
+                or game.get("isCodeRedemptionOnly")
+            ):
+                continue
+
+            start_dates = [
+                g.context.context.value.get("startDate").split(".")[0]
+                for g in parser_now.find(game)
+            ]
+            end_dates = [
+                g.context.context.value.get("endDate").split(".")[0]
+                for g in parser_now.find(game)
+            ]
+
+            image_urls = [
+                image.get("url")
+                for image in game.get("keyImages")
+                if image.get("type") == "Thumbnail" and image.get("url")
+            ]
+            image_url = image_urls[0] if image_urls else None
+
+            info_dict = {
+                "game_id": game.get("id"),
+                "title": game.get("title"),
+                "desc": game.get("description"),
+                "url": f"https://store.epicgames.com/en-US/p/{game.get('productSlug')}",
+                "start_date": datetime.strptime(start_dates[0], "%Y-%m-%dT%H:%M:%S")
+                if start_dates
+                else None,
+                "end_date": datetime.strptime(end_dates[0], "%Y-%m-%dT%H:%M:%S")
+                if end_dates
+                else None,
+                "thumbnail_url": image_url,
+            }
+
+            info = cls(**info_dict)
+            infos.append(info)
+        return infos
+
+    def embed(self):
+        """Return an embed"""
+        embed = Embed(title=self.title, description=self.desc, url=self.url)
+        embed.set_image(url=self.thumbnail_url)
+        embed.set_footer(
+            text=f"Started on {self.start_date.strftime(r'%Y-%m-%d')}\nEnds on {self.end_date.strftime(r'%Y-%m-%d')}"
+        )
+        return embed
