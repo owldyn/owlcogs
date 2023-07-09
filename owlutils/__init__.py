343e5987e474367afdddb4d958e2df0ab63fd5f8
owldyn <owldyn@gmail.com>
2023-07-09 07:41:52 +0000

    add snooper

--- owlutils/__init__.py
+++ owlutils/__init__.py
@@ -1,9 +1,9 @@
 from .owlutils import OwlUtils
+from .snooper import StatusSnooper
 
-__red_end_user_data_statement__ = (
-    "This cog only stores information that is explicitly given by a user."
-)
+__red_end_user_data_statement__ = "This cog stores information that is explicitly given by a user, as well as their status history."
 
 
 async def setup(bot):
     await bot.add_cog(OwlUtils(bot))
+    await bot.add_cog(StatusSnooper(bot))
---
+++ owlutils/snooper.py
@@ -0,0 +1,122 @@
+import logging
+import time
+from typing import Dict, Union
+
+import discord
+from redbot.core import Config, app_commands, commands
+
+
+class StatusSnooper(commands.Cog):
+    """Snoops statuses and allows users to see last online time"""
+
+    default_global_settings = {"users": {}}
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.conf = Config.get_conf(self, identifier=4007)
+        self.conf.register_global(**self.default_global_settings)
+        self.log = logging.getLogger("StatusSnooper")
+        self.ctx_menu = app_commands.ContextMenu(name="Last Online", callback=self.last_online)
+        self.bot.tree.add_command(self.ctx_menu)
+
+    @commands.Cog.listener("on_presence_update")
+    async def update_status(self, before: discord.Member, after: discord.Member):
+        """Save a status update"""
+        async with self.conf.users() as users:
+            user_id = str(after.id)
+            current_data: Dict[str, list] = users.get(user_id, {})
+            self._init_user(current_data)
+            if before.status != after.status:
+                self.log.debug(
+                    "%s changed from %s to %s!", after.name, before.status, after.status
+                )
+                current_data["status"].append(
+                    {
+                        "before": before.raw_status,
+                        "after": after.raw_status,
+                        "timestamp": int(time.time()),
+                    }
+                )
+
+            if before.activity != after.activity:
+                self.log.debug(
+                    "%s's activity changed from %s to %s!",
+                    after.name,
+                    str(before.activity),
+                    str(after.activity),
+                )
+                current_data["activity"].append(
+                    {
+                        "before": str(before.activity),
+                        "after": str(after.activity),
+                        "timestamp": int(time.time()),
+                    }
+                )
+            self.log.debug(current_data)
+            users[user_id] = current_data
+
+    def _init_user(self, current_data):
+        if not current_data.get("status"):
+            current_data["status"] = []
+        if not current_data.get("activity"):
+            current_data["activity"] = []
+
+    @app_commands.guild_only()
+    async def last_online(
+        self, ctx: discord.Interaction, member: Union[discord.Member, discord.User]
+    ):
+        """Sends when the user was last online or offline."""
+
+        user_id = member.id
+        async with self.conf.users() as users:
+            info = users.get(str(user_id))
+        # Need to get the user from the guild because for some reason
+        # The member passed to the function shows as offline?
+        member = next((m for m in ctx.guild.members if m.id == user_id), member)
+        currently_online = member.status == discord.Status.online
+        current = -1
+        try:
+            statuses = info.get("status")
+            last = statuses[current]
+        except (AttributeError, IndexError):
+            await ctx.response.send_message(
+                "I don't have any history on that user.", ephemeral=True
+            )
+            return
+
+        def while_logic(last_status):
+            online_options = ["online", "idle", "dnd"]
+            offline_options = ["offline", "invisible"]
+            if currently_online:
+                return (
+                    last_status.get("before") not in offline_options
+                    and last_status.get("after") not in offline_options
+                )
+            return (
+                last_status.get("before") not in online_options
+                and last_status.get("after") not in online_options
+            )
+
+        while while_logic(last):
+            try:
+                current -= 1
+                last = statuses[current]
+            except IndexError:
+                last = None
+                break
+        if not last:
+            await ctx.response.send_message(
+                f"That user has never been {'online' if not currently_online else 'offline'} in my history!",
+                ephemeral=True,
+            )
+            return
+        if member.status == discord.Status.online:
+            await ctx.response.send_message(
+                f"{member.display_name} has been online since <t:{last.get('timestamp')}:R>.",
+                ephemeral=True,
+            )
+            return
+        await ctx.response.send_message(
+            f"{member.display_name} was last online at <t:{last.get('timestamp')}:R>.",
+            ephemeral=True,
+        )
