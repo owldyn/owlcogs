0e6c3165b1a7a6e0e0e2ca76bba2ca4e9c0bea5e
owldyn <owldyn@gmail.com>
2024-10-30 03:38:17 +0000

    fix timezone issue

--- owlutils/timestamp.py
+++ owlutils/timestamp.py
@@ -1,12 +1,12 @@
 import logging
 import re
-from datetime import datetime, tzinfo
+from datetime import datetime
+from typing import cast
 
 import discord
 import zoneinfo
 from dateutil.parser import parse
 from dateutil.tz import UTC
-from discord.ext import tasks
 from redbot.core import Config, app_commands, commands
 from redbot.core.bot import Red
 from zoneinfo import ZoneInfo
@@ -25,10 +25,13 @@ class Timestamp(commands.Cog):
 
     async def get_timezones(self, interaction, current):
         selections = sorted(
-            (app_commands.Choice(name=z, value=z)
-            for z in zoneinfo.available_timezones()
-            if current.lower() in z.lower())
-        , key=lambda x: x.value)
+            (
+                app_commands.Choice(name=z, value=z)
+                for z in zoneinfo.available_timezones()
+                if current.lower() in z.lower()
+            ),
+            key=lambda x: x.value,
+        )
         return selections[:25]
 
     @timestamps.command(name="set_timezone")
@@ -51,18 +54,24 @@ class Timestamp(commands.Cog):
             match = re.search(r"\<([^\>]*)\>", message.content)
             if match:
                 try:
-                    parsed_time = parse(match.group(1))
+                    parsed_time = parse(match.group(1)).replace(tzinfo=UTC)
 
                     ctx: commands.Context = await self.bot.get_context(message)
                     try:
-                        timezone = await self.conf.user(message.author).get_raw("timezone")
+                        timezone = cast(
+                            str,
+                            await self.conf.user(message.author).get_raw("timezone"),
+                        )
                     except Exception:
                         await ctx.reply(
                             "You haven't set up your timezone yet! Do that with /timestamps set_timezone."
                         )
                         return
+                    offset = parsed_time.astimezone(ZoneInfo(timezone)).utcoffset()
+
                     time = int(
-                        parsed_time.timestamp() - parsed_time.astimezone(ZoneInfo(timezone)).utcoffset().total_seconds()
+                        parsed_time.timestamp()
+                        - (offset.total_seconds() if offset else 0)
                     )
                     await ctx.reply(f"<t:{time}:f>", mention_author=False)
 
