7d374ffcd2a478a7620fe18a22d119f5465ff579
owldyn <owldyn@gmail.com>
2024-10-30 02:12:05 +0000

    add timestamps

--- owlutils/__init__.py
+++ owlutils/__init__.py
@@ -1,6 +1,7 @@
 from .countdown import Countdown
 from .owlutils import OwlUtils
 from .snooper import StatusSnooper
+from .timestamp import Timestamp
 
 __red_end_user_data_statement__ = "This cog stores information that is explicitly given by a user, as well as their status history."
 
@@ -9,3 +10,4 @@ async def setup(bot):
     await bot.add_cog(OwlUtils(bot))
     await bot.add_cog(StatusSnooper(bot))
     await bot.add_cog(Countdown(bot))
+    await bot.add_cog(Timestamp(bot))
---
+++ owlutils/timestamp.py
@@ -0,0 +1,70 @@
+import logging
+import re
+from datetime import datetime, tzinfo
+
+import discord
+import zoneinfo
+from dateutil.parser import parse
+from dateutil.tz import UTC
+from discord.ext import tasks
+from redbot.core import Config, app_commands, commands
+from redbot.core.bot import Red
+from zoneinfo import ZoneInfo
+
+
+class Timestamp(commands.Cog):
+    def __init__(self, bot: Red):
+        self.bot = bot
+        self.conf = Config.get_conf(self, identifier=4007)
+        self.log = logging.getLogger("owlutils.timestamp")
+
+    timestamps = app_commands.Group(
+        name="timestamps",
+        description="Automatic timestamps!",
+    )
+
+    async def get_timezones(self, interaction, current):
+        selections = sorted(
+            (app_commands.Choice(name=z, value=z)
+            for z in zoneinfo.available_timezones()
+            if current.lower() in z.lower())
+        , key=lambda x: x.value)
+        return selections[:25]
+
+    @timestamps.command(name="set_timezone")
+    @app_commands.autocomplete(timezone=get_timezones)
+    async def set_timezone(self, ctx: discord.Interaction, timezone: str):
+        try:
+            tz = ZoneInfo(timezone)
+        except Exception:
+            await ctx.response.send_message("That timezone doesn't exist!")
+            return
+        await self.conf.user(ctx.user).set({"timezone": timezone})
+        tz = ZoneInfo(await self.conf.user(ctx.user).get_raw("timezone"))
+        await ctx.response.send_message(
+            f"Done! Your current time should be {datetime.now(tz).strftime(r'%Y-%m-%d %H:%M:%S')}"
+        )
+
+    @commands.Cog.listener("on_message_without_command")
+    async def print_timezone(self, message: discord.Message):
+        if "<" in message.content and ">" in message.content:
+            match = re.search(r"\<([^\>]*)\>", message.content)
+            if match:
+                try:
+                    parsed_time = parse(match.group(1))
+
+                    ctx: commands.Context = await self.bot.get_context(message)
+                    try:
+                        timezone = await self.conf.user(message.author).get_raw("timezone")
+                    except Exception:
+                        await ctx.reply(
+                            "You haven't set up your timezone yet! Do that with /timestamps set_timezone."
+                        )
+                        return
+                    time = int(
+                        parsed_time.timestamp() - parsed_time.astimezone(ZoneInfo(timezone)).utcoffset().total_seconds()
+                    )
+                    await ctx.reply(f"<t:{time}:f>", mention_author=False)
+
+                except Exception as e:
+                    return
